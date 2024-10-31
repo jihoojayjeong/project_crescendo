99e5dd40394b751df7f89a9e13b12933532a22d3
Jay <jghdg1234@gmail.com>
2024-10-31 14:42:22 -0400

    update

--- frontend/src/pages/loginPage.js
+++ frontend/src/pages/loginPage.js
@@ -41,7 +41,7 @@ const LoginPage = () => {
                 <div className="max-w-md w-full space-y-8">
                     <div>
                         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-                            Welcome to Crescendo!
+                            Welcome to Crescendo!!!~~~~
                         </h2>
                         <p className="mt-2 text-center text-sm text-gray-600">
                             Please log in to access the system
